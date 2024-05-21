595037852e6e3b70f0c72ae046a09cbfc9ad1a98
mengpeng <pengmeng@yangqianguan.com>
2024-05-21 19:20:17 +0800

    fix: 补充三方库

--- tsup.config.ts
+++ tsup.config.ts
@@ -14,5 +14,5 @@ export default defineConfig({
   external: [
     'vscode',
   ],
-  noExternal:["tailwindcss","css-to-tailwindcss"]
+  noExternal:["tailwindcss","css-to-tailwindcss","handlebars","mkdirp","rimraf"]
 })
